b254f9ca32ce15f2cc6b97541c43250a25f6df4d
Dead_Skater666 <kristina192003@gmail.com>
2023-01-10 07:50:33 +0600

    style admin page

--- src/components/pages/AdminPage.jsx
+++ src/components/pages/AdminPage.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import AddModels from '../Models/AddModels';
-
+import "../pages/AdminPage.css"
 const AdminPage = () => {
     return (
         <div>
-            Admin page
             <AddModels/>
+            <img className='backAdmin' src="https://hyperpc.ru/images/product/cyber/gallery/hpc-cyber-gallery-black-alt.jpg" alt="" />
         </div>
     );
 };
